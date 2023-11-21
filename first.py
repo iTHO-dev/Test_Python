95bfc99db607cbbac6d4e1393d5f57d58048ca3c
iTHO <hannes.otter@itho.at>
2023-11-21 10:51:57 +0100

    new file:   database.db 	new file:   first.py 	new file:   make_db.sql

---
+++ first.py
@@ -0,0 +1,18 @@
+import shutil
+
+text = """Shutil copyfile() Methode zum Kopieren von Dateien in Python\ntext"""
+
+print("Text:",text)
+
+# Shutil copyfile() Methode zum Kopieren von Dateien in Python
+# Es kopiert das Quellmaterial in eine Datei mit dem Zielnamen.
+# Wenn das Ziel nicht beschreibbar ist, tritt während des Kopiervorgangs
+# eine IOError-Ausnahme auf. Wenn die Ursprungs- und die Zieldatei gleich
+# sind, kehrt SameFileError zurück.
+# Die Pfadnamen der Quell- und Zieldatei werden als Zeichenketten angegeben.
+
+#import shutil
+#source = r"C:\Users\DelftStack\Documents\test\test.txt"
+#destination = r"C:\Users\DelftStack\Pictures\test2\test2.txt"
+#shutil.copyfile(source, destination)
+
---
+++ make_db.sql
@@ -0,0 +1,8 @@
+-- SQLite
+CREATE TABLE aufgaben (
+    id int,
+    bezeichnung text,
+    prio int,
+    start varchar(10),
+    end varchar(10)
+);
