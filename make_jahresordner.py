f8ad21fbdba41a3d334d01a80e11f608196951c8
iTHO <hannes.otter@itho.at>
2023-11-22 21:20:20 +0100

    modified:   make_jahresordner.py

--- make_jahresordner.py
+++ make_jahresordner.py
@@ -1,6 +1,8 @@
 import os
 import calendar
 import locale
+import shutil
+import glob
 
 
 def create_year_month_folders(year):
@@ -9,14 +11,14 @@ def create_year_month_folders(year):
         if not os.path.exists(folder):
             os.makedirs(folder)
     
-        # Setze die Sprache auf Deutsch
+        # Setze die Sprache auf Deutsch, damit calendar.month_name[month] Deutsche Monatsnamen ausgibt
     locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
 
     for folder in folders:
         year_folder_path = os.path.join(folder, str(year))
         if not os.path.exists(year_folder_path):
             os.makedirs(year_folder_path)
-        
+        20
         for month in range(1, 13):
             month_name = f'{month:02d}'  # Formatierung für zweistellige Monatsnummern (z.B. 01, 02, ..., 12)
             month_name = month_name + ' ' + calendar.month_name[month]
@@ -25,11 +27,26 @@ def create_year_month_folders(year):
                 os.makedirs(month_folder_path)
                 print(f"Monatsordner {month_folder_path} wurde erstellt.")
 
+def copy_excel_files(year):
+    # Finde Excel-Dateien im aktuellen Verzeichnis
+    excel_files = glob.glob("Vorlage*.xlsx") + glob.glob("Vorlage*.xls")
+    
+    for excel_file in excel_files:
+        # Zielverzeichnis für Belege und Kontrolle
+        destination_kontrolle = os.path.join("Kontrolle Monate", str(year))
+
+        # Kopiere Excel-Datei in das Kontrolle-Verzeichnis
+        if not os.path.exists(destination_kontrolle):
+            os.makedirs(destination_kontrolle)
+        shutil.copy(excel_file, destination_kontrolle)
+
 def main():
     try:
         year = int(input("Bitte geben Sie das gewünschte Jahr ein: "))
         create_year_month_folders(year)
         print(f"Ordnerstruktur für das Jahr {year} wurde erstellt.")
+        copy_excel_files(year)
+        print("Excel-Dateien wurden in den Jahresordner für Kontrolle kopiert.")
     except ValueError:
         print("Ungültige Eingabe für das Jahr. Bitte geben Sie eine gültige Jahreszahl ein.")
 
