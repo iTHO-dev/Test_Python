3a70d5ac8b8114a28bc63fd7467a2ed137d0bd40
iTHO <hannes.otter@itho.at>
2023-11-22 21:42:28 +0100

    modified:   make_jahresordner.py

--- make_jahresordner.py
+++ make_jahresordner.py
@@ -11,14 +11,14 @@ def create_year_month_folders(year):
         if not os.path.exists(folder):
             os.makedirs(folder)
     
-        # Setze die Sprache auf Deutsch, damit calendar.month_name[month] Deutsche Monatsnamen ausgibt
+    # Setze die Sprache auf Deutsch, damit calendar.month_name[month] Deutsche Monatsnamen ausgibt
     locale.setlocale(locale.LC_TIME, "de_DE.UTF-8")
 
     for folder in folders:
         year_folder_path = os.path.join(folder, str(year))
         if not os.path.exists(year_folder_path):
             os.makedirs(year_folder_path)
-        20
+        
         for month in range(1, 13):
             month_name = f'{month:02d}'  # Formatierung für zweistellige Monatsnummern (z.B. 01, 02, ..., 12)
             month_name = month_name + ' ' + calendar.month_name[month]
@@ -35,10 +35,18 @@ def copy_excel_files(year):
         # Zielverzeichnis für Belege und Kontrolle
         destination_kontrolle = os.path.join("Kontrolle Monate", str(year))
 
+        # Überprüfe, ob die Datei bereits im Zielordner vorhanden ist
+        #file_exists_belege = os.path.exists(os.path.join(destination_belege, excel_file.replace('Vorlage', '')))
+        file_exists_kontrolle = os.path.exists(os.path.join(destination_kontrolle, excel_file.replace('Vorlage ', '')))
+
+
         # Kopiere Excel-Datei in das Kontrolle-Verzeichnis
-        if not os.path.exists(destination_kontrolle):
-            os.makedirs(destination_kontrolle)
-        shutil.copy(excel_file, destination_kontrolle)
+        if not file_exists_kontrolle:
+            if not os.path.exists(destination_kontrolle):
+                os.makedirs(destination_kontrolle)
+            new_name = os.path.join(destination_kontrolle, excel_file.replace('Vorlage ', ''))
+            shutil.copy(excel_file, new_name)
+            print(f"Excel-Dateien {new_name} wurde in den Jahresordner für Kontrolle kopiert.")
 
 def main():
     try:
@@ -46,7 +54,6 @@ def main():
         create_year_month_folders(year)
         print(f"Ordnerstruktur für das Jahr {year} wurde erstellt.")
         copy_excel_files(year)
-        print("Excel-Dateien wurden in den Jahresordner für Kontrolle kopiert.")
     except ValueError:
         print("Ungültige Eingabe für das Jahr. Bitte geben Sie eine gültige Jahreszahl ein.")
 
